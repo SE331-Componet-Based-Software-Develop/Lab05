8a550d6eeff12d8c1e0d57b776d417f1b52af033
MirrorML <2867405686@qq.com>
2022-08-03 22:50:29 +0800

    Finish

--- src/views/event/EventEditView.vue
+++ src/views/event/EventEditView.vue
@@ -1,9 +1,25 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="edit">Edit Me</button>
 </template>
 
 <script>
 export default {
-  props: ['id', 'event']
+  props: ['id', 'event'],
+  inject: ['GStore'],
+  methods: {
+    edit() {
+      this.GStore.flashMessage =
+        'The data has been updated ! You are successfully edit for' +
+        this.event.title
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
